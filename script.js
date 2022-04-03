c5f09e729e212f48aabfed69228d5bb36b46fdc6
Ngawang Luding <ngluding@gmail.com>
2022-04-03 02:52:18 -0400

    First attempt at a project with UI

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+let startGame = () => {
+    const images = document.querySelectorAll("img");
+    images.forEach((img) => {
+        img.addEventListener("click", () => {
+            if (img.className) {
+                playRound(img.className);
+
+            }
+        })
+    })
+}
+
+let resetGame = () => {
+    const pScore = document.querySelector(".playerScore")
+    const cScore = document.querySelector(".computerScore")
+    const tScore = document.querySelector(".tieScore")
+    const pChoice = document.querySelector(".playerChoice")
+    const cChoice = document.querySelector(".computerChoice")
+    const result = document.querySelector(".result")
+
+    pScore.textContent = `Player Score: 0`
+    cScore.textContent = `Computer Score: 0`
+    tScore.textContent = `Tie(s): 0`
+    pChoice.textContent = "Player Choice:"
+    cChoice.textContent = "Computer Choice:"
+    result.textContent = "Result:"
+
+    playerScore = 0;
+    compScore = 0;
+    tie = 0;
+
+    document.querySelector(".modal").style.display = "none";
+}
+
 let computerPlay = () => {
     const randomNumber = Math.floor(Math.random()*3);
 
@@ -6,97 +40,103 @@ let computerPlay = () => {
         } else if (randomNumber === 1) {
             return ("paper");
         } else {
-            return ("scisscor");
+            return ("scissor");
         }
-}
-
-let playerPlay = () => {
-    let userInput = prompt("Choose rock, paper or scisscor:");
-    if (userInput === null) {
-        alert("Ok try next time then :)")
-    }
-    userInput = userInput.toLowerCase();
-    return userInput;
 }
 
 let playerScore = 0;
 let compScore = 0;
 let tie = 0;
 
+let scoreUpdate = () => {
+    const pScore = document.querySelector(".playerScore")
+    const cScore = document.querySelector(".computerScore")
+    const tScore = document.querySelector(".tieScore")
+
+    pScore.textContent = `Player Score: ${playerScore}`
+    cScore.textContent = `Computer Score: ${compScore}`
+    tScore.textContent = `Tie(s): ${tie}`
+}
+
 let win = () => {
     playerScore++;
-    console.log("You won!");
-    console.log(`Your score is ${playerScore}, computer score is ${compScore} and there is ${tie} ties`);
-    console.log("------------------------------------------------------------")
+    scoreUpdate ();
+    const winResult = document.querySelector(".result")
+    winResult.textContent = "Result: You Won!"
 }
 
 let lose = () => {
     compScore++;
-    console.log("You lost!");
-    console.log(`Your score is ${playerScore}, computer score is ${compScore} and there is ${tie} ties`);
-    console.log("------------------------------------------------------------")
+    scoreUpdate ();
+    const loseResult = document.querySelector(".result")
+    loseResult.textContent = "Result: You Lost!"
 }
 
 let draw = () => {
     tie++;
-    console.log("It's a draw");
-    console.log(`Your score is ${playerScore}, computer score is ${compScore} and there is ${tie} ties`);
-    console.log("------------------------------------------------------------")
-}
-
-let roundResults = () => {
-    console.log(`You selected: ${playerPlay()}`);
-    console.log(`Computer selected: ${computerPlay()}`);
-
+    scoreUpdate ();
+    const tieResult = document.querySelector(".result")
+    tieResult.textContent = "Result: You Tied!"
 }
 
 let gameWinner  = () => {
-    if (playerScore > compScore) {
-        console.log(`You won the game! Your score was ${playerScore} and computer score was ${compScore}`);
-    } else if (compScore > playerScore) {
-        console.log(`You lost the game! Your score was ${playerScore} and computer score was ${compScore}`);
-    } else {
-        console.log(`It was a tie since both you and computer had the same score of ${playerScore}`);
+    const winner = Math.max(playerScore, compScore);
+    if(winner >= 5) {
+        document.querySelector(".modal").style.display = "flex";
+        return;
     }
 }
 
-let playRound = () => {
-    const playerSelection = playerPlay();
-    const computerSelection = computerPlay();
-
-    let roundResults = () => {
-    console.log(`You selected: ${playerSelection}`);
-    console.log(`Computer selected: ${computerSelection}`);
+let displayEnd = () => {
+    if(playerScore === 5) {
+        document.querySelector(".winner").textContent = `You Won! Your score was ${playerScore} and computer score was ${compScore}`;
+    } else {
+        document.querySelector(".winner").textContent = `You Lost! Your score was ${playerScore} and computer score was ${compScore}`;
     }
+}
+
+let playRound = (pSelection) => {
+    let playerSelection = pSelection;
+    let computerSelection = computerPlay();
+
+    document.querySelector(`#${computerSelection}`).classList.add("active");
+
+    setTimeout(() => {
+        document.querySelector(`#${computerSelection}`).classList.remove("active");
+    }, 1000);
+
+    const pChoice = document.querySelector(".playerChoice")
+    const cChoice = document.querySelector(".computerChoice")
+    pChoice.textContent = `Player Choice: ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`
+    cChoice.textContent = `Computer Choice: ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`
 
     switch (playerSelection + computerSelection) {
-        case "rockscisscor": 
+        case "rockscissor": 
         case "paperrock":
         case "scissorpaper":
-            roundResults();
             win();
+            gameWinner();
             break;
         case "rockpaper":
         case "scissorrock":
         case "paperscissor":
-            roundResults();
             lose();
+            gameWinner();
             break;
         case "rockrock":
         case "scissorscissor":
         case "paperpaper":
-            roundResults();
             draw();
+            gameWinner();
             break;
     }
-}
-
-let game = () => {
-    for (let i = 0; i < 5; i++) {
-        playRound();
+    
+    if(playerScore == 5) {
+        document.querySelector(".winner").innerHTML = "You Won!" + "<br/>" + "<br/>" + "Your score was " + playerScore + " and computer score was " + compScore; 
+    } else {
+        document.querySelector(".winner").innerHTML = "You Lost!" + "<br/>" + "<br/>" + "Your score was " + playerScore + " and computer score was " + compScore; 
     }
-    gameWinner();
+
 }
 
-game();
-
+startGame();
